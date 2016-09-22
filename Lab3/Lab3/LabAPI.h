c3ffea3e11846dff7e80677207bb9cd28ebe4860
Willster419 <willster419@outlook.com>
2016-09-22 15:45:13 -0400

    in b4 major change

--- Lab3/Lab3/LabAPI.h
+++ Lab3/Lab3/LabAPI.h
@@ -2,15 +2,13 @@
 // Willard Wider               //
 // 09/21/16                    //
 // LabAPI header file          //
-// Last Modified 09/21/16      //
+// Last Modified 09/22/16      //
 /////////////////////////////////
 //basic start to the program
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <ctime>
-#include <time.h>
-#include <stdio.h>
+#include <cstdlib>
 using namespace std;
 ostringstream oss;//extra string stream to allow for other types than strings to be added to an output
 
@@ -19,4 +17,32 @@ void print(string str)
 {
 	cout << str << "\n";
 	oss.str("");
+}
+
+//method to simplifing the procedure of printing to the console
+void print(unsigned char c)
+{
+	cout << c << "\n";
+	oss.str("");
+}
+
+//returns the size of the character array of characters that are in use
+int size(char temp[])
+{
+	int tempp = 0;
+	while(temp[tempp] !=0 )
+	{
+		tempp++;
+	}
+	return tempp;
+}
+//returns the size of the character array of characters that are in use
+int size(unsigned char temp[])
+{
+	int tempp = 0;
+	while(temp[tempp] !=0 )
+	{
+		tempp++;
+	}
+	return tempp;
 }
--- Lab3/Lab3/Main.cpp
+++ Lab3/Lab3/Main.cpp
@@ -9,84 +9,147 @@
 #include <string>
 #include <sstream>
 #include <ctime>
-#include <time.h>
-#include <stdio.h>
-#include <iso646.h>
+#include "labAPI.h"
+#include <cstdlib>
+#define KEY_OFFSET 33
+#define KEY_BITMASK 69
 using namespace std;
-ostringstream oss;//extra string stream to allow for other types than strings to be added to an output
 //declare all requried variables for this project
-int key = 0;
-int xorKey = 42;//'*'
+unsigned char key, xorKey, theChar, numAdded, sizeOfMessage;
+bool hasString = false;
+char input[420];
 
-void setKey()
+//sets a key char based on a random number generator seeded with the time function, therefore always
+void setKeys(char theXorKey, bool encryptOrDecrypt)
 {
-	key = time(NULL);
-	srand(key);
-	key = rand() %420;
+	//true = encrypt, false = decrypt
+	unsigned char temp;
+	if(encryptOrDecrypt)
+	{
+		temp = input[size(input)-1];
+		sizeOfMessage = 0;
+		srand(temp);
+		key = rand();
+		xorKey = theXorKey;
+		input[size(input)] = ++xorKey;
+		input[size(input)] = ++key;
+		
+	}
+	else
+	{
+		//need to set the xorKey, sizeOfMessage, and key
+		xorKey = size(input)/2;
+		key = (size(input)/2) +1;
+		xorKey--;
+		key--;
+		sizeOfMessage = (size(input)/2) -1;
+	}
+	
+
+}
+
+unsigned char phaseOne(bool encryptOrDecrypt, unsigned char c)
+{
+	//true = encrypting, false = decrypting
+	if (encryptOrDecrypt)
+	{
+		numAdded = 1;
+		c += key;
+		while (32 > c || c > 127)
+		{
+			c += key;
+			numAdded++;
+		}
+		numAdded += KEY_OFFSET;
+		input[size(input)+ sizeOfMessage++]=numAdded;
+		return c;
+	}
+	else
+	{
+		numAdded = input[size(input)-1 - sizeOfMessage];
+		numAdded -= KEY_OFFSET;
+		while ( numAdded != 0)
+		{
+			c -= key;
+			numAdded--;
+		}
+		c -=key;
+		input[size(input)-1 - sizeOfMessage--] = 0;
+		return c - key;
+	}
+	return -1;
+}
+
+//xor bit logic. second phase of encryption
+unsigned char phaseTwo(unsigned char c)
+{
+	c = (c ^= xorKey);
+	return c;
+}
+
+//bit inversion. the third phase of encryption
+unsigned char phaseThree(unsigned char c)
+{
+	c = ~c;
+	return c;
+}
+
+//stuff
+unsigned char encrypt(unsigned char inputValue)
+{
+	//generate key
+	//encrypt key
+	//encrypt message
+	//attack encrypted key
+	unsigned char tmep;
+	//bitwise negate
+	tmep = phaseThree(inputValue);
+	//bitwise xor
+	tmep = phaseTwo(tmep);
+	//randomize the int using specific key to multiply
+	tmep = phaseOne(true,tmep);
+	return tmep;
+}
+
+//more stuff
+unsigned char decrypt(unsigned char inputValue)
+{
+	unsigned char temp;
+	//unrandomize
+	temp = phaseOne(false,inputValue);
+	//bitwise xor
+	temp = phaseTwo(temp);
+	//bitwise negative
+	temp = phaseThree(temp);
+	return temp;
 }
 
 //main entry point of the application
 int main()
 {
-	//step 1: convert to int
-	//C in asci is 67 in decimal
-	char theChar = 'C';
-	char theIntOfChar = -1;
-	theIntOfChar = theChar;//char -> int
+	//encryption
+	print("enter a string");
+	cin.getline(input,420);
+	setKeys(input[0],true);
+	int currentSize = size(input)-1;
+	for(int i = 0; i < currentSize; i++)
+	{
+		input[i] = encrypt(input[i]);
+	}
+	oss <<"your encrypted string is " << input;
+	print (oss.str());
 
-	//step 2: randomize the int using specific key to multiply
-	setKey();
-	theIntOfChar = (theIntOfChar * key) %128;//randomize the int
-	
+	//decryption
+	setKeys(input[0],false);
+	currentSize = size(input)/2;
+	for(int i = 0; i < currentSize; i++)
+	{
+		input[i] = decrypt(input[i]);
+	}
+	oss <<"your decrypted string is " << input;
+	print (oss.str());
+	print ("press enter to continue...");
+	cin.getline(input,420);
 
-	//step 3: use bitwise xor
-	theIntOfChar = ( theIntOfChar ^= xorKey);
-
-	//step 4: use bitwise negate
-	theIntOfChar = ~theIntOfChar;
-
-	//step 5: back to char for encrypted value
-	char temp2 = theIntOfChar;
-	
-	//step 6: back to int
-	theIntOfChar = temp2;
-	
-	//step 7: bitwise negate
-	theIntOfChar = ~theIntOfChar;
-
-	//step 8: bitwise xor
-	theIntOfChar = ( theIntOfChar ^= xorKey);
-
-	//step 9: unrandomize the int using key to divide
-	theIntOfChar = theIntOfChar / key;
-	
-	//step 10: back to char
-	
-	char temp;
-	temp = (char) theIntOfChar;//int -> char
-	cout << temp;
 	return 0;
-}
-
-void phaseOneEncryption()
-{
-	/*setKey();
-	int encryptedInt = theIntOfChar * key;//randomize the int
-	theIntOfChar = encryptedInt / key;*/
-}
-
-void phaseTwoEncryption()
-{
-	/*
-	theChar = ( theChar ^= xorKey);
-	theChar = ( theChar ^= xorKey);
-	*/
-}
-
-void phaseThreeEncryption()
-{
-	/*
-	theChar = ~theChar;
-	theChar = ~theChar;
-	*/
 }
