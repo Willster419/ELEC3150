30ac2406ebe77ae47b1d13d3fb160ebe0dfdffcd
Willster419 <willster419@outlook.com>
2016-09-22 22:17:02 -0400

    that's about as much work as i plan to put into this project

--- Lab3/Lab3/Main.cpp
+++ Lab3/Lab3/Main.cpp
@@ -11,145 +11,172 @@
 #include <ctime>
 #include "labAPI.h"
 #include <cstdlib>
-#define KEY_OFFSET 33
 #define KEY_BITMASK 69
 using namespace std;
-//declare all requried variables for this project
-unsigned char key, xorKey, theChar, numAdded, sizeOfMessage;
-bool hasString = false;
-char input[420];
 
-//sets a key char based on a random number generator seeded with the time function, therefore always
-void setKeys(char theXorKey, bool encryptOrDecrypt)
+//even more stuff
+unsigned char addInRange(unsigned char low, unsigned char high, unsigned char currentPOS, unsigned char add)
 {
-	//true = encrypt, false = decrypt
-	unsigned char temp;
-	if(encryptOrDecrypt)
+	if(high >= currentPOS+add)
 	{
-		temp = input[size(input)-1];
-		sizeOfMessage = 0;
-		srand(temp);
-		key = rand();
-		xorKey = theXorKey;
-		input[size(input)] = ++xorKey;
-		input[size(input)] = ++key;
-		
+		return currentPOS+add;
 	}
-	else
-	{
-		//need to set the xorKey, sizeOfMessage, and key
-		xorKey = size(input)/2;
-		key = (size(input)/2) +1;
-		xorKey--;
-		key--;
-		sizeOfMessage = (size(input)/2) -1;
-	}
-	
-
+	unsigned char answer = (currentPOS+add) - (high+1);
+	unsigned char room = high - low;
+	answer = answer%room;
+	return low+answer;
 }
 
-unsigned char phaseOne(bool encryptOrDecrypt, unsigned char c)
+//even more cool stuff
+unsigned char subInRange(short low, short high, short currentPOS, short add)
+{
+	if(low <= currentPOS-add)
+	{
+		return currentPOS-add;
+	}
+	short answer = (currentPOS-add) - (low-1);
+	//answer++;
+	answer = answer*-1;
+	short room = high - low;
+	answer = answer%room;
+	return high-answer;
+}
+
+//sets a key char based on a random number generator seeded with the time function, therefore always
+unsigned char generateKey()
+{
+	srand(time(0));
+	return rand();
+}
+
+unsigned char bitMask(bool encryptOrDecrypt, unsigned char inputChar, unsigned char amount)
 {
 	//true = encrypting, false = decrypting
 	if (encryptOrDecrypt)
 	{
-		numAdded = 1;
-		c += key;
-		while (32 > c || c > 127)
+		
+		inputChar = addInRange(32,126,inputChar,amount);
+		return inputChar;
+		
+		/*inputChar += amount;
+		while (32 > inputChar || inputChar > 127)
 		{
-			c += key;
-			numAdded++;
+			inputChar += amount;
 		}
-		numAdded += KEY_OFFSET;
-		input[size(input)+ sizeOfMessage++]=numAdded;
-		return c;
+		return inputChar;*/
 	}
 	else
 	{
-		numAdded = input[size(input)-1 - sizeOfMessage];
-		numAdded -= KEY_OFFSET;
-		while ( numAdded != 0)
+		inputChar = subInRange(32,126,inputChar,amount);
+		return inputChar;
+		/*
+		inputChar -= amount;
+		while (32 > inputChar || inputChar > 127)
 		{
-			c -= key;
-			numAdded--;
-		}
-		c -=key;
-		input[size(input)-1 - sizeOfMessage--] = 0;
-		return c - key;
+			inputChar -= amount;
+		}*/
+		return inputChar;
 	}
 	return -1;
 }
 
 //xor bit logic. second phase of encryption
-unsigned char phaseTwo(unsigned char c)
+unsigned char xor(unsigned char c,unsigned char key)
 {
-	c = (c ^= xorKey);
+	c = (c ^= key);
 	return c;
 }
 
 //bit inversion. the third phase of encryption
-unsigned char phaseThree(unsigned char c)
+unsigned char invert(unsigned char c)
 {
-	c = ~c;
-	return c;
+	return ~c;
 }
 
 //stuff
-unsigned char encrypt(unsigned char inputValue)
+string encrypt(string input)
 {
 	//generate key
+	unsigned char ukey = generateKey();
+	unsigned char key = ukey;
 	//encrypt key
+	//key = xor(key,KEY_BITMASK);
+	//key = invert(key);
+	key = bitMask(true,key,KEY_BITMASK);
 	//encrypt message
-	//attack encrypted key
-	unsigned char tmep;
-	//bitwise negate
-	tmep = phaseThree(inputValue);
-	//bitwise xor
-	tmep = phaseTwo(tmep);
-	//randomize the int using specific key to multiply
-	tmep = phaseOne(true,tmep);
-	return tmep;
+	for(int i = 0; i < input.size(); i++)
+	{
+		//input[i] = xor(input[i],ukey);
+		//input[i] = invert(input[i]);
+		//input[i] = bitMask(true,input[i],xor(input[i],ukey));
+		//input[i] = bitMask(true,input[i],invert(ukey));
+		input[i] = bitMask(true,input[i],ukey);
+	}
+	//attach encrypted key
+	input += key;
+	return input;
 }
 
 //more stuff
-unsigned char decrypt(unsigned char inputValue)
+string decrypt(string input)
 {
-	unsigned char temp;
-	//unrandomize
-	temp = phaseOne(false,inputValue);
-	//bitwise xor
-	temp = phaseTwo(temp);
-	//bitwise negative
-	temp = phaseThree(temp);
-	return temp;
+	//remove encrypted key
+	unsigned char key = input[input.size()-1];
+	//decrypt key
+	key = bitMask(false,key,KEY_BITMASK);
+	//key = invert(key);
+	//key = xor(key,KEY_BITMASK);
+	//decrypt message
+	for(int i = 0; i < input.size()-1; i++)
+	{
+		input[i] = bitMask(false,input[i],key);
+		//input[i] = bitMask(false,input[i],invert(key));
+		//input[i] = bitMask(false,input[i],xor(key,key));
+		//input[i] = invert(input[i]);
+		//input[i] = xor(input[i],key);
+	}
+	input = input.substr(0,input.size()-1);
+	return input;
 }
 
 //main entry point of the application
 int main()
 {
-	//encryption
-	print("enter a string");
-	cin.getline(input,420);
-	setKeys(input[0],true);
-	int currentSize = size(input)-1;
-	for(int i = 0; i < currentSize; i++)
+	bool done = false;
+	while (!done)
 	{
-		input[i] = encrypt(input[i]);
+		//menu to select to encrypt, decrypt, exit
+		print("type encrypt to encrypt a message,");
+		print("type decrypt to dectypt a message");
+		print("type exit to exit");
+		string input;
+		getline(cin,input);
+		if(input == "encrypt")
+		{
+			print("type message to encrypt");
+			getline(cin,input);
+			//encrypt
+			print("your encrypted message is " + encrypt(input));
+			print("press enter to continue...");
+			getline(cin,input);
+		}
+		else if (input == "decrypt")
+		{
+			print("type message to decrypt");
+			getline(cin,input);
+			//decrypt
+			print("your decrypted message is " + decrypt(input));
+			print("press enter to continue...");
+			getline(cin,input);
+		}
+		else if (input == "exit")
+		{
+			done = true;
+		}
+		else
+		{
+			print("invalid input");
+		}
 	}
-	oss <<"your encrypted string is " << input;
-	print (oss.str());
-
-	//decryption
-	setKeys(input[0],false);
-	currentSize = size(input)/2;
-	for(int i = 0; i < currentSize; i++)
-	{
-		input[i] = decrypt(input[i]);
-	}
-	oss <<"your decrypted string is " << input;
-	print (oss.str());
-	print ("press enter to continue...");
-	cin.getline(input,420);
-
 	return 0;
 }
