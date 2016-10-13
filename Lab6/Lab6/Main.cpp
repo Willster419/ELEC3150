8e4df7a471ad2d04d2e9727ddb2036d5f735635d
Willster419 <willster419@outlook.com>
2016-10-13 12:46:23 -0400

    updated LabAPI.h and added base of lab 6

---
+++ Lab6/Lab6/Main.cpp
@@ -0,0 +1,34 @@
+/////////////////////////////////
+// Willard Wider               //
+// XX/XX/XX                    //
+// Lab X                       //
+// Last Modified XX/XX/XX      //
+/////////////////////////////////
+
+//includes
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include "LabAPI.h"
+#include <cstdlib>
+
+//defines
+
+
+//constants
+
+
+//method prototypes
+
+
+//other definitions
+using namespace std;
+
+
+//the main entry point of the application
+int main()
+{
+
+	return 0;
+}
